22943a4c27d34872420eded504a10e07e6ac4e9b
mario <ma.palacios.m@icloud.com>
2020-07-16 03:02:31 -0400

    init tools

---
+++ source_code/src/main/java/com/ecom/product/tools/CurrencyCLP.java
@@ -0,0 +1,50 @@
+package com.ecom.product.tools;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+//TODO: mover esto a MS-ORDER
+
+@Component
+public class CurrencyCLP {
+
+    public BigDecimal addRoundingDecimals(BigDecimal total, BigDecimal percentage){
+        if(percentage.compareTo(new BigDecimal(0)) == 1){
+            return total.add(this.calculatePercentage(total, percentage)).setScale(0, RoundingMode.CEILING);
+        }
+        else {
+            return total;
+        }
+    }
+
+    public BigDecimal substractRoundingDecimals(BigDecimal total, BigDecimal percentage){
+        if(percentage.compareTo(new BigDecimal(0)) == 1){
+            return total.subtract(this.calculatePercentage(total, percentage)).setScale(0, RoundingMode.CEILING);
+        }
+        else {
+            return total;
+        }
+    }
+
+    public BigDecimal roundClp(BigDecimal total){
+        BigDecimal remainder = total.remainder(new BigDecimal(10));
+
+        if( (remainder.compareTo(new BigDecimal(1)) == 0 || remainder.compareTo(new BigDecimal(1)) == 1) &&
+                (remainder.compareTo(new BigDecimal(5)) == 0 || remainder.compareTo(new BigDecimal(5)) == -1) ){
+            return total.subtract(remainder);
+        }
+        if( (remainder.compareTo(new BigDecimal(6)) == 0 || remainder.compareTo(new BigDecimal(6)) == 1) &&
+                (remainder.compareTo(new BigDecimal(9)) == 0 || remainder.compareTo(new BigDecimal(9)) == -1) ){
+            return total.add(new BigDecimal(10).subtract(remainder));
+        }
+        return total;
+    }
+
+    private BigDecimal calculatePercentage(BigDecimal value, BigDecimal percentage){
+        return (percentage.multiply(value)).divide(new BigDecimal(100));
+    }
+
+
+}
